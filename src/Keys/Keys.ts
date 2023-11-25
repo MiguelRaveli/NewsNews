54051ec913c91f239a1a2824c6ab83ad1852c297
unknown <miguelraveli123@gmail.com>
2023-11-25 18:50:48 -0300

    atualizando http

--- src/Keys/Keys.ts
+++ src/Keys/Keys.ts
@@ -1,3 +1,3 @@
 export const endpointNews = "https://newsapi.org/v2/";
-export const endpointWeather = "http://api.weatherapi.com/v1/current.json?key=5a5f02ef3f7247fbbe9184724231111&q=London&aqi=no";
+export const endpointWeather = "https://api.weatherapi.com/v1/current.json?key=5a5f02ef3f7247fbbe9184724231111&q=London&aqi=no";
 export const keyWeather = "5a5f02ef3f7247fbbe9184724231111"
